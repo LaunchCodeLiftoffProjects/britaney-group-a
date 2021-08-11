432fa52417a970580ddf657413bbb83971d8c2b9
Sridevi15k <sridevi15k@gmail.com>
2021-08-10 23:11:02 -0500

    Added Gradle, SpringBoot with libraries

---
+++ src/main/java/org/launchcode/britaneygroupa/BritaneyGroupAApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.britaneygroupa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BritaneyGroupAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BritaneyGroupAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/srilc101javafullstackproject/SriLc101JavafullstackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.srilc101javafullstackproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriLc101JavafullstackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
