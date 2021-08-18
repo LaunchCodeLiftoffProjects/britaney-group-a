db9ff3b966957b69f3996b600fecec4d9cb1f09b
Ncognxto <c.hicks2929@gmail.com>
2021-08-18 15:57:48 -0500

    Updated WebApplicationConfig

--- src/main/java/org/launchcode/britaneygroupa/WebApplicationConfig.java
+++ src/main/java/org/launchcode/britaneygroupa/WebApplicationConfig.java
@@ -1,4 +1,18 @@
 package org.launchcode.britaneygroupa;
 
-public class WebApplicationConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebApplicationConfig implements WebMvcConfigurer {
+
+    @Bean
+    public AuthenticationFilter authenticationFilter() {
+        return new AuthenticationFilter();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor( authenticationFilter() );
+    }
 }
