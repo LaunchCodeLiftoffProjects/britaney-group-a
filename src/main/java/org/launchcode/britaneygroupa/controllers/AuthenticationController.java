1700d278e6f615771eca01cd8aed6fbe160391fb
Ncognxto <c.hicks2929@gmail.com>
2021-08-18 14:55:05 -0500

    Updated AuthenticationController

--- src/main/java/org/launchcode/britaneygroupa/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/britaneygroupa/controllers/AuthenticationController.java
@@ -120,4 +120,11 @@ public class AuthenticationController {
 
         return "redirect:";
     }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "redirect:/login";
+    }
+
 }
