784ac580f38e3debacebf55b3db01ba98c8a41f3
Ncognxto <c.hicks2929@gmail.com>
2021-08-18 14:09:23 -0500

    Updated AuthenticationController

--- src/main/java/org/launchcode/britaneygroupa/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/britaneygroupa/controllers/AuthenticationController.java
@@ -1,4 +1,37 @@
 package org.launchcode.britaneygroupa.controllers;
 
+import org.launchcode.britaneygroupa.models.User;
+import org.launchcode.britaneygroupa.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Controller
 public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
 }
