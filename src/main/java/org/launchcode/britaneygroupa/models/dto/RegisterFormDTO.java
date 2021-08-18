a37e16989bea3089e41ac97d1517f1c22d64e6e1
Ncognxto <c.hicks2929@gmail.com>
2021-08-18 14:16:45 -0500

    Updated RegisterFormDTO

--- src/main/java/org/launchcode/britaneygroupa/models/dto/RegisterFormDTO.java
+++ src/main/java/org/launchcode/britaneygroupa/models/dto/RegisterFormDTO.java
@@ -1,4 +1,14 @@
 package org.launchcode.britaneygroupa.models.dto;
 
-public class RegisterFormDTO {
+public class RegisterFormDTO extends LoginFormDTO {
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
 }
