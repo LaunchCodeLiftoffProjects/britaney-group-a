bdddeafad8b3e14e2f5583e9221b2c6c747d858c
Sridevi15k <sridevi15k@gmail.com>
2021-08-31 17:18:44 -0500

    Redirect add product page after submit to product list page

--- src/main/java/org/launchcode/britaneygroupa/controllers/ProductController.java
+++ src/main/java/org/launchcode/britaneygroupa/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -37,18 +38,20 @@ public class ProductController {
     }
 
     @PostMapping("add")
-    public String addProduct(@ModelAttribute @Valid Product newProduct, Errors errors, HttpServletRequest request) {
+    public String addProduct(@ModelAttribute @Valid Product newProduct, Errors errors, HttpServletRequest request, RedirectAttributes redirectAttrs) {
         if (errors.hasErrors()) {
             return "addProduct";
         }
 
         // get user id from session
-        newProduct.setUserId((Integer) request.getSession().getAttribute("user"));
+        newProduct.setUserId(((User) request.getSession().getAttribute("user")).getId());
 
         // save product to db
         productRepository.save(newProduct);
 
-        return "addProduct";
+        redirectAttrs.addFlashAttribute("info", "Product Added");
+
+        return "redirect:/products";
 
     }
 }
