90030030750eef7d26ef37c74a17e99db9503ce5
sangi <sssangeetha494@gmail.com>
2021-09-20 19:34:06 -0500

    updated image add button

--- src/main/java/org/launchcode/britaneygroupa/controllers/ProductController.java
+++ src/main/java/org/launchcode/britaneygroupa/controllers/ProductController.java
@@ -6,16 +6,23 @@ import org.launchcode.britaneygroupa.models.data.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.io.*;
+import java.nio.file.*;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
+
 
     @Autowired
     ProductRepository productRepository;
@@ -37,7 +44,7 @@ public class ProductController {
     }
 
     @PostMapping("add")
-    public String addProduct(@ModelAttribute @Valid Product newProduct, Errors errors, HttpServletRequest request, RedirectAttributes redirectAttrs) {
+    public String addProduct(@ModelAttribute @Valid Product newProduct, Errors errors, HttpServletRequest request, RedirectAttributes redirectAttrs, @RequestParam("image") MultipartFile multipartFile) throws IOException {
         if (errors.hasErrors()) {
             return "addProduct";
         }
@@ -45,11 +52,25 @@ public class ProductController {
         // get user id from session
         newProduct.setUserId(((User) request.getSession().getAttribute("user")).getId());
 
-        // save product to db
-        productRepository.save(newProduct);
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        newProduct.setFileName(fileName);
+        Product savedProduct = productRepository.save(newProduct);
+        String uploadDir = "Product-image/" + savedProduct.getId();
+        Path uploadPath = Paths.get(uploadDir);
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new IOException("Could not save image file: " + fileName, ioe);
+        }
+
+            // save product to db
+        //productRepository.save(newProduct);
 
         redirectAttrs.addFlashAttribute("info", "Product Added");
-
         return "redirect:/products";
     }
 
@@ -60,11 +81,26 @@ public class ProductController {
     }
 
     @PostMapping("/edit/{id}")
-    public String edit(@PathVariable(name = "id") int id, @ModelAttribute @Valid Product newProduct, Errors errors, HttpServletRequest request, RedirectAttributes redirectAttrs) {
+    public String edit(@PathVariable(name = "id") int id, @ModelAttribute @Valid Product newProduct, Errors errors, HttpServletRequest request, RedirectAttributes redirectAttrs,@RequestParam("image") MultipartFile multipartFile) throws IOException {
         newProduct.setUserId(((User) request.getSession().getAttribute("user")).getId());
         // save product to db
         newProduct.setId(id);
-        productRepository.save(newProduct);
+
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        newProduct.setFileName(fileName);
+        Product savedProduct = productRepository.save(newProduct);
+        String uploadDir = "Product-image/" + savedProduct.getId();
+        Path uploadPath = Paths.get(uploadDir);
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new IOException("Could not save image file: " + fileName, ioe);
+        }
+        //productRepository.save(newProduct);
 
         redirectAttrs.addFlashAttribute("info", "Product Saved");
 
@@ -76,4 +112,7 @@ public class ProductController {
         model.addAttribute("product", productRepository.deleteById(id));
         return "redirect:/products";
     }
-  }
+
+
+
+}
