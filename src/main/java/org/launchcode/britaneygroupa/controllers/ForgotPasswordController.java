0d5a75c1f2b91d08b62d12c8a43e26faa706996e
Ncognxto <c.hicks2929@gmail.com>
2021-09-20 20:15:31 -0500

    Added @Controller to ForgotPasswordController

--- src/main/java/org/launchcode/britaneygroupa/controllers/ForgotPasswordController.java
+++ src/main/java/org/launchcode/britaneygroupa/controllers/ForgotPasswordController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 
+@Controller
 public class ForgotPasswordController {
     @Autowired
     private JavaMailSender mailSender;
