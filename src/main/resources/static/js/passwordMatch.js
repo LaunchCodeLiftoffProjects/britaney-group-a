82517abb636aaac0e315f4c970ee286dcb86fb5c
Sridevi15k <sridevi15k@gmail.com>
2021-09-27 11:52:48 -0500

    Added validation for passwords match.

--- src/main/resources/static/js/passwordMatch.js
+++
@@ -1,8 +0,0 @@
-
-//function checkPasswordMatch(fieldConfirmPassword) {
-//    if (fieldConfirmPassword.value != $("#password").val()) {
-//        fieldConfirmPassword.setCustomValidity("Passwords do not match!");
-//    } else {
-//        fieldConfirmPassword.setCustomValidity("");
-//    }
-//}
